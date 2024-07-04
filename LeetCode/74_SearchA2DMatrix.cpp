8beedfa79ba144f11f89330e17b4109cc344fcdb
Lakshit Jain <jainlakshit849@gmail.com>
2024-07-04 16:04:18 +0530

    Medium, Binary Search

---
+++ LeetCode/74_SearchA2DMatrix.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question Link : https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int low = 0;
+        int high = matrix.size() - 1;
+        int last = matrix[0].size() - 1;
+        int current = -1;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (target >= matrix[mid][0] && target <= matrix[mid][last])
+            {
+                current = mid;
+                break;
+            }
+            else if (target < matrix[mid][0])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        if (current == -1)
+        {
+            return false;
+        }
+
+        low = 0;
+        high = last;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (target == matrix[current][mid])
+            {
+                return true;
+            }
+            else if (target < matrix[current][mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return false;
+    }
+};
