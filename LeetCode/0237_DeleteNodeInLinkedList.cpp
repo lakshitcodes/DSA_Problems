911f987c5200b66cee8b792493a12b5afdc1000a
Lakshit Jain <jainlakshit849@gmail.com>
2024-07-07 19:09:01 +0530

    Medium, Linked List

---
+++ LeetCode/0237_DeleteNodeInLinkedList.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question Link : https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+//  Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);
+        node->val = node->next->val;
+        node->next = node->next->next;
+        return;
+    }
+};
