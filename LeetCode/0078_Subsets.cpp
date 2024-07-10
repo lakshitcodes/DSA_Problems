13c7e4db0822a4b2b3a5dda7902dd024536d16f8
Lakshit Jain <jainlakshit849@gmail.com>
2024-07-10 09:28:13 +0530

    Using Bit Manipulation

--- LeetCode/0078_Subsets.cpp
+++ LeetCode/0078_Subsets.cpp
@@ -26,3 +26,27 @@ public:
         return ans;
     }
 };
+
+// Bit Manipulation
+
+class Solution
+{
+public:
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        int n = nums.size();
+        int p = 1 << n;
+        vector<vector<int>> ans(p);
+        for (int i = 0; i < p; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if ((i >> j) & 1)
+                {
+                    ans[i].push_back(nums[j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
