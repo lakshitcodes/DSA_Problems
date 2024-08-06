ea82e09d86b3cf54de99149dba279e92000bdf9a
Lakshit Jain <jainlakshit849@gmail.com>
2024-08-06 10:20:52 +0530

    Easy

---
+++ LeetCode/3014_MinimumNumberOfPushesToTypeWordI.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question Link :  https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i/
+
+class Solution
+{
+public:
+    int minimumPushes(string word)
+    {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);
+
+        vector<int> count(26, 0);
+        for (auto &i : word)
+        {
+            count[i - 'a']++;
+        }
+        sort(count.rbegin(), count.rend());
+        int ans = 0;
+        for (int i = 0; i < 26; i++)
+        {
+            ans += count[i] * (i / 8 + 1);
+        }
+        return ans;
+    }
+};
