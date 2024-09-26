2b882c7bca3745e79b7d52daf68336d0275029d1
Lakshit Jain <jainlakshit849@gmail.com>
2024-09-26 13:22:26 +0530

    POTD 26 September 2024

---
+++ LeetCode/0729_MyCalendarI.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Question Link : https://leetcode.com/problems/my-calendar-i/
+
+class MyCalendar {
+public:
+    vector<pair<int, int>> ranges;
+    MyCalendar() {}
+    bool book(int start, int end) {
+        for (auto const& pair : ranges) {
+            int r_start = pair.first;
+            int r_end = pair.second;
+            if (start < r_end && end > r_start)
+                return false;
+        }
+        ranges.push_back(make_pair(start, end));
+        return true;
+    }
+};
