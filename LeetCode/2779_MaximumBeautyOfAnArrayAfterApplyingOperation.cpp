2e89565d7748bd7ccd31485b94914233d8be9d3f
Lakshit Jain <jainlakshit849@gmail.com>
2024-12-11 16:03:04 +0530

    POTD 11 December 2024

---
+++ LeetCode/2779_MaximumBeautyOfAnArrayAfterApplyingOperation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Question Link : https://leetcode.com/problems/maximum-beauty-of-an-array-after-applying-operation/
+
+class Solution {
+public:
+    static int maximumBeauty(vector<int>& nums, int k) {
+        int freq[100001]={0};
+        int xMax=0, xMin=1e6;
+        for(int x: nums){
+            freq[x]++;
+            xMax=max(x, xMax);
+            xMin=min(x, xMin);
+        }
+        int cnt=0, maxCnt=0;
+        for(int l=xMin, r=xMin; r<=xMax; r++){
+            cnt+=freq[r];
+            while(r-l>2*k){
+                cnt-=freq[l];
+                l++;
+            }
+            maxCnt=max(maxCnt, cnt);
+        }
+        return maxCnt;
+    }
+};
