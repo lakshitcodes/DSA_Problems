59a353c01f30588fe5e6722ccc205d93169a140e
Lakshit Jain <jainlakshit849@gmail.com>
2024-12-26 09:57:12 +0530

    POST CONTEST

--- Codeforces/2024.12.22 - Round 995 (Div 3)/D_Counting_Pairs.cpp
+++ Codeforces/2024.12.22 - Round 995 (Div 3)/D_Counting_Pairs.cpp
@@ -15,37 +15,66 @@ using namespace std;
 #define sum(arr) accumulate(arr.begin(),arr.end(),0LL)
 #define take(arr) for(auto &i : arr) cin>>i;
 
-int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    int t;
-    cin >> t;
-    while (t--) {
-        int n,x,y;
-        cin >> n >> x >> y;
-        vi arr(n);
+int bs1(int i,vll&a,ll x,ll sum){
+    int low=i+1;
+    int high=a.size()-1;
+    int ind=-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(sum-a[mid]>=x){
+            ind=mid;
+            low=mid+1;
+           
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return ind;
+}
+int bs2(int i,vll&a,ll y,ll sum){
+    int low=i+1;
+    int high=a.size()-1;
+    int ind=-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(sum-a[mid]<=y){
+            ind=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ind;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int t=0;
+    cin>>t;
+    while(t--){
+        ll n,x,y;
+        cin>>n>>x>>y;
+        vll a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        sort(a);
         ll sum=0;
         for(int i=0;i<n;i++){
-            cin >> arr[i];
-            sum+=arr[i];
+            sum+=a[i];
         }
-
-        sort(arr);
-        ll ans=0;
+        ll cnt=0;
         for(int i=0;i<n-1;i++){
-            ll remSum = sum-arr[i];
-
-            ll maxPoss = remSum-x;
-            ll minPoss = remSum-y;
-
-            auto low = lower_bound(arr.begin()+i+1,arr.end(),minPoss);
-            auto high = upper_bound(arr.begin()+i+1,arr.end(),maxPoss);
-
-            ll temp = high-low;
-            ans+=max(temp,0LL);
+            sum-=a[i];
+            int hi=bs1(i,a,x,sum);
+            int si=bs2(i,a,y,sum);
+            if(si==-1||hi==-1)cnt+=0;
+            else cnt+=(hi-si+1);
+            sum+=a[i];
         }
-        cout << ans << endl;
-
+        cout<<cnt<<endl;
     }
     return 0;
-}   
+}
