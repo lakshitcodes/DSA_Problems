0ccdf19014595119c854463b9afa502458c9733c
Lakshit Jain <jainlakshit849@gmail.com>
2025-01-18 11:36:08 +0530

    Post Contest CF Round 997

--- Codeforces/2025.01.17 - Round 997 (Div 2)/B_Find_the_Permutation.cpp
+++ Codeforces/2025.01.17 - Round 997 (Div 2)/B_Find_the_Permutation.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-
+#include<bits/stdc++.h>
 using namespace std;
 
 bool comparator(int a, int b, const vector<string>& graph) {
@@ -14,25 +10,23 @@ int main() {
     cin >> t; 
     while (t--) {
         int n;
-        cin >> n;
-        vector<string> graph(n);
-
-        for (int i = 0; i < n; i++) {
-            cin >> graph[i];
+        cin>>n;
+        vector<string>adj(n);
+        vector<int>ans(n); 
+        for(int i=0;i<n;i++){
+            cin>>adj[i];
         }
-
-        vector<int> permutation(n);
-        for (int i = 0; i < n; i++) {
-            permutation[i] = i + 1;
-        }
-        sort(permutation.begin(), permutation.end(), [&](int a, int b) {
-            return comparator(a, b, graph);
-        });
-
-        for (int i = n-1; i >=0; i--) {
-            cout << permutation[i] << " ";
-        }
-        cout << endl;
+        iota(ans.begin(), ans.end(), 0); 
+        sort(ans.begin(), ans.end(), [&](int u, int v) { 
+        if(u<v) { 
+        return adj[u][v]=='1'; 
+        } 
+        return adj[v][u]=='0'; 
+        }); 
+        for (int i = 0; i < n; i++) { 
+        cout << ans[i] +1<<" "; 
+        } 
+        cout<<endl;
     }
 
     return 0;
