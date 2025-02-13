02ed92fde8dc705f2e114878f6895b56b913e98b
Lakshit Jain <jainlakshit849@gmail.com>
2025-02-13 15:37:36 +0530

    Codechef Starters 173 Div 2

---
+++ Codechef/2025.02.12 - Starters 173 Div 2/1_Time_Penalty.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define ii pair<int, int>
+#define vii vector<ii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vll vector<long long>
+#define take(arr) for(auto &i : arr) cin>>i;
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    ll x,y;
+    cin>>x>>y;
+    cout<<x + (y*10)<<endl;
+    return 0;
+}
