8d0cc4bedb1851558eee27a96408f6669be76b3d
Lakshit Jain <jainlakshit849@gmail.com>
2025-04-09 20:49:53 +0530

    Codechef Starters 181B

---
+++ Codechef/2025.04.09 - Starters 181 Div 2/0_Move_Grid.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question Link : https://www.codechef.com/START181B/problems/MOVEMENT
+
+int main() {
+	int a,b,c,d;
+	cin>>a>>b>>c>>d;
+	cout<<a-c<<" "<<b-d<<endl;
+
+}
