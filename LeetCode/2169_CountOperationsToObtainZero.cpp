b81924bf6bfb8104b5b4f8ecf4ec8645b3f21fd7
Lakshit Jain <jainlakshit849@gmail.com>
2025-11-09 10:54:59 +0530

    POTD 9 November 2025

---
+++ LeetCode/2169_CountOperationsToObtainZero.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question Link : https://leetcode.com/problems/count-operations-to-obtain-zero/
+
+class Solution
+{
+public:
+    int countOperations(int num1, int num2)
+    {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);
+
+        int ans = 0;
+        while (num1 && num2)
+        {
+            if (num1 > num2)
+            {
+                ans += num1 / num2;
+                num1 %= num2;
+            }
+            else
+            {
+                ans += num2 / num1;
+                num2 %= num1;
+            }
+        }
+        return ans;
+    }
+};
