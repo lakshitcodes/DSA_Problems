5d3f7103850cc4f21fa2b50e5fc8b303fee88e9f
Lakshit Jain <jainlakshit849@gmail.com>
2025-11-16 11:31:45 +0530

    POTD 16 November 2025

---
+++ LeetCode/1513_NumberOfSubstringsWithOnly1s.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question Link : https://leetcode.com/problems/number-of-substrings-with-only-1s
+
+class Solution
+{
+public:
+    int numSub(string s)
+    {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);
+
+        int ans = 0;
+        int ones = 0;
+        const int mod = 1e9 + 7;
+        for (auto &i : s)
+        {
+            if (i == '0')
+            {
+                ones = 0;
+            }
+            else
+            {
+                ones++;
+                ans = (ans + ones) % mod;
+            }
+        }
+        return ans;
+    }
+};
